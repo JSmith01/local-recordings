6d6ce3e6c00bbcd9469d03ba1d818083024facbb
Dmitry Zlygin <dzlygin@gmail.com>
2022-12-11 12:27:22 +0400

    added screensharing button

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,30 @@ import { loadImage, makeImageCircled } from './utils';
 import RecordingFacade from './RecordingFacade';
 
 const btnCapture = document.getElementById('capture') as HTMLButtonElement;
+const btnShare = document.getElementById('share') as HTMLButtonElement;
 const btnRecord = document.getElementById('record') as HTMLButtonElement;
 const btnStop = document.getElementById('stop') as HTMLButtonElement;
 const chkHd = document.getElementById('hd-check') as HTMLInputElement;
 const videoPreview = document.getElementById('preview') as HTMLVideoElement;
 const recPreview = document.getElementById('rec-preview') as HTMLVideoElement;
+
+const rp = new RecordingFacade({
+    producer: { width: 720, height: 405, frameRate: 30 },
+    recorder: {
+        videoBitsPerSecond: 2_500_000,
+        audioBitsPerSecond: 128_000,
+    },
+});
+
+(window as unknown as Record<string, unknown>).rp = rp;
+recPreview.srcObject = rp.outputStream;
+
+const placeholder = loadImage('./placeholder.webp').then(makeImageCircled);
+rp.addTile('a', 'User 1', placeholder);
+
+rp.startRendering();
+
+const stopStream = (stream: MediaStream) => stream.getTracks().forEach(track => track.stop());
 
 btnCapture.onclick = () => {
     if (videoPreview.srcObject) return;
@@ -18,9 +37,45 @@ btnCapture.onclick = () => {
     });
 };
 
+let sharing = false, sharingStreamPromise: Promise<MediaStream>, sharingStream: MediaStream;
+
+async function stopSharing() {
+    if (sharingStreamPromise) {
+        await sharingStreamPromise;
+    }
+    if (sharingStream) {
+        rp.removeStream('sharing');
+        rp.removeTile('sharing');
+        stopStream(sharingStream);
+    }
+    sharing = false;
+    btnShare.innerText = 'share';
+}
+
+btnShare.onclick = async () => {
+    if (sharing) {
+        stopSharing();
+        return;
+    }
+
+    sharing = true;
+    sharingStreamPromise = navigator.mediaDevices.getDisplayMedia({
+        audio: true,
+        video: {
+            width: { max: 1920 },
+            height: { max: 1200 },
+        }
+    });
+    sharingStream = await sharingStreamPromise;
+    sharingStreamPromise = undefined;
+    btnShare.innerText = 'stop share';
+    rp.addTile('sharing', 'User 1 sharing', placeholder, sharingStream, true);
+    sharingStream.getVideoTracks()[0].onended = stopSharing;
+};
+
 btnStop.onclick = async () => {
     if (videoPreview.srcObject) {
-        (videoPreview.srcObject as MediaStream).getTracks().forEach(track => track.stop());
+        stopStream(videoPreview.srcObject as MediaStream);
         videoPreview.srcObject = null;
     }
 
@@ -45,23 +100,5 @@ btnRecord.onclick = async () => {
     if (!outputStream) return;
 
     rp.setWriter(outputStream);
-    rp.start();
+    await rp.start();
 };
-
-const rp = new RecordingFacade({
-    producer: { width: 720, height: 405, frameRate: 30 },
-    recorder: {
-        videoBitsPerSecond: 2_500_000,
-        audioBitsPerSecond: 128_000,
-    },
-});
-
-(window as unknown as Record<string, unknown>).rp = rp;
-recPreview.srcObject = rp.outputStream;
-
-const placeholder = loadImage('./placeholder.webp').then(makeImageCircled);
-rp.addTile('a', 'Test tile', placeholder);
-
-// since DEFAULT_PLACEHOLDER init is async too
-setTimeout(() => rp.draw(), 10);
-placeholder.then(() => rp.draw());
